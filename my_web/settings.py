595388b5f1b432b33408a0e81c3aeb14d78a25c0
mukeshsaravanan <mukeshsaravanan003@gmail.com>
2021-07-12 19:00:37 +0000

    remove old st uff

--- my_web/settings.py
+++ my_web/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-(z-cpotyv)^f+6!#j5n32&by20m-4gpu1d@0ji38k(d1(bbtpi
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['mukeshsaravanan.pythonanywhere.com']
 
 
 # Application definition
@@ -120,8 +120,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
-MEDIA_URL ='/media/' 
-MEDIA_ROOT = BASE_DIR / 'media' 
+STATIC_ROOT = BASE_DIR / 'static'
+
+MEDIA_URL ='/media/'
+MEDIA_ROOT = BASE_DIR / 'media'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
